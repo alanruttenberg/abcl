798babb40f5fafee6664c1172ad7a39a18399b3c
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-01-22 08:03:31 -0500

    leftover merge

--- contrib/jss/transform-to-field.lisp
+++ contrib/jss/transform-to-field.lisp
@@ -15,14 +15,14 @@
 ;;     #"{foo}.baz" -> (get-java-field (find-java-class foo) "baz" t)
 
 
-(defun jss-transform-to-field (string)
+(defun jss-transform-to-field (string sharp-arg)
   (let* ((pattern (#"compile" 'java.util.regex.Pattern "(.*)\\.([^.]+)$"))
 	 (matcher (#"matcher" pattern string)))
     (#"find" matcher)
     (let ((parts (list (#"group" matcher 1) (#"group" matcher 2))))
       (check-class-or-eval (first parts))
       (check-field-or-eval (second parts))
-      (apply 'field-access-expression parts))))
+      (apply 'field-access-expression sharp-arg parts ))))
 
 ;; http://stackoverflow.com/questions/5205339/regular-expression-matching-fully-qualified-class-names
 (defun check-class-or-eval (string)
@@ -37,11 +37,26 @@
 	  (string)
 	  "inside #\"..\" expected either a field name or an expression surrounded by {}. Found: #~s" string))
 
-(defun field-access-expression (thing field)
-  `(get-java-field ,(if (char= (char thing 0) #\{)
-			(intern (string-upcase (subseq thing 1 (- (length thing) 1))))
-			`(load-time-value (find-java-class ,thing)))
-		   ,(if (char= (char field 0) #\{)
-			(intern (string-upcase (subseq field 1 (- (length field) 1))))
-			field)
-		   t))
+(defun field-access-expression (sharp-arg thing field)
+  (if (and (not (char= (char thing 0) #\{)) (not (char= (char field 0) #\{)))
+      (static-field-ref-transform thing field sharp-arg)
+      `(get-java-field ,(if (char= (char thing 0) #\{)
+			    (read-from-string (subseq thing 1 (- (length thing) 1)))
+			    `(load-time-value (find-java-class ,thing)))
+		       ,(if (char= (char field 0) #\{)
+			    (read-from-string (subseq field 1 (- (length field) 1)))
+			    field)
+		       t)))
+
+;; If a class name and explicit field name we can look everything up at load time
+(defun static-field-ref-transform (class field sharp-arg)
+  `(,(if (eql sharp-arg 0) 'jcall-raw 'jcall) (load-time-value (jmethod "java.lang.reflect.Field" "get" "java.lang.Object"))
+	  (load-time-value 
+	     (let ((jfield (find-declared-field ,field (find-java-class ,class))))
+	       (#"setAccessible" jfield t)
+	       jfield))
+    (load-time-value (find-java-class ',class))))
+ 
+
+
+ 
