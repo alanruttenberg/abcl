bfeef0f30ae5b8ccd2205d805dc9aaf13baf6675
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-02-11 15:07:33 -0500

    def-java-parse -> def-java-read (maybe should be generate?). Rework using defmethod so you can specialize generation by using different class

--- contrib/jss/ast.lisp
+++ contrib/jss/ast.lisp
@@ -1,48 +1,32 @@
 (in-package :jss)
 
 (defvar *javaparsers* (make-hash-table :test 'equalp))
+(defvar *class-to-last-component* (make-hash-table :test 'equalp))
 
-(defmacro def-javaparse (ast-class fields &body body)
-  (let ((class (find-java-class (concatenate 'string "com.github.javaparser.ast.expr." (string ast-class))))
-	(fname (intern (format nil "PARSE-~a" (string-upcase (string ast-class))))))
-    `(setf (gethash ,class *javaparsers*)
-	   (flet ((,fname (node)
-		    (let ,(loop for field in fields 
-				for jfield across (#"getDeclaredFields" class)
-				collect (list field `#"{node}.{,jfield}"))
-		      ,@body)))
-	     #',fname))))
+(defclass sharp-quote-expression-reader () ())
 
-(def-javaparse LongLiteralExpr nil
-  (#"getValue" node))
+(defmacro def-java-read (ast-class class fields &body body)
+  (let ((jclass (find-java-class (concatenate 'string "com.github.javaparser.ast.expr." (string ast-class)))))
+    `(progn
+       (setf (gethash ,jclass *class-to-last-component*) ',ast-class)
+       (defmethod ,ast-class ((obj ,class) node) 
+	 (let ,(loop for field in fields 
+		     for jfield across (#"getDeclaredFields" jclass)
+		     collect (list field `#"{node}.{,jfield}"))
+	   ,@body)))))
 
-(def-javaparse BooleanLiteralExpr (value)
-  (#"getValue" node))
-(def-javaparse IntegerLiteralExpr nil
-  (#"getValue" node))
-(def-javaparse DoubleLiteralExpr nil
-  (#"getValue" node))
-(def-javaparse CharLiteralExpr nil
-  (#"getValue" node))
-(def-javaparse StringLiteralExpr nil
-  (#"getValue" node))
-(def-javaparse NullLiteralExpr nil
-  +null+)
-
-(def-javaparse ObjectCreationExpr (scope type typeArguments arguments anonymousClassBody)
-  (declare (ignore anonymousClassBody typeArguments scope))
-  `(new ',(ast-to-sexp (#"getName" type)) ,@(mapcar 'ast-to-sexp (j2list arguments)))
-  )
-    
-(defun ast-to-sexp (node)
+(defmethod ast-to-sexp ((r sharp-quote-expression-reader) node)
   (when (jinstance-of-p  node "java.util.Optional")
     (setq node (#"get" node)))
   (when (or (equal node (load-time-value (#"empty"  'java.util.Optional ))) (null node))
     (return-from ast-to-sexp nil))
-  (funcall (gethash (jobject-class node) *javaparsers*) node))
+  (funcall (gethash (jobject-class node) *class-to-last-component*)  r node))
 
-(defun read-java-expression (expression)
-  (ast-to-sexp (#"parseExpression" 'javaparser expression)))
+(defmethod read-java-expression ((r sharp-quote-expression-reader) expression)
+  (ast-to-sexp r (#"parseExpression" 'javaparser expression)))
+
+(defun read-sharp-quote-expression (string)
+  (read-java-expression (make-instance 'sharp-quote-expression-reader) string))
 
 (defun maybe-class (el)
   (if (and (symbolp el) (upper-case-p (char (string el) 0)))
@@ -51,62 +35,100 @@
 	  (intern (string-upcase el))
 	  el)))
 
-(def-javaparse MethodCallExpr (scope typeArguments name arguments)
+(def-java-read LongLiteralExpr sharp-quote-expression-reader nil
+  (read-from-string (#"replaceFirst" (#"getValue" node) "L" "")))
+
+(def-java-read BooleanLiteralExpr sharp-quote-expression-reader (value)
+  (if (equal value "true") t nil))
+
+(def-java-read IntegerLiteralExpr sharp-quote-expression-reader nil
+ (parse-integer (#"getValue" node)))
+
+(def-java-read DoubleLiteralExpr sharp-quote-expression-reader nil
+  (let ((raw (#"getValue" node)))
+    (setq raw (#"replaceAll" raw "_" ""))
+    (if (#"matches" raw ".*[dD]$")
+	(read-from-string (#"replaceFirst" (subseq raw 0 (1- (length raw))) "e" "d"))
+	(if (#"matches" raw ".*[fF]$")
+	    (read-from-string (subseq raw 0 (1- (length raw))))
+	    (read-from-string raw)))))
+
+(def-java-read CharLiteralExpr sharp-quote-expression-reader nil
+  (#"getValue" node))
+
+(def-java-read StringLiteralExpr sharp-quote-expression-reader nil
+  (#"getValue" node))
+
+(def-java-read NullLiteralExpr sharp-quote-expression-reader nil
+  +null+)
+
+(def-java-read ObjectCreationExpr sharp-quote-expression-reader (scope type typeArguments arguments anonymousClassBody)
+  (declare (ignore anonymousClassBody typeArguments scope))
+  `(new ',(ast-to-sexp obj (#"getName" type)) ,@(mapcar 'ast-to-sexp obj (j2list arguments)))
+  )
+
+(def-java-read MethodCallExpr sharp-quote-expression-reader (scope typeArguments name arguments)
   (declare (ignore typeArguments))
-  (let* ((scope1 (ast-to-sexp scope))
+  (let* ((scope1 (ast-to-sexp obj scope))
 	 (how  (if (and (symbolp scope1) (not (null scope1)) (upper-case-p (char (string scope1) 0))) 
 		  'jstatic
 		  'jcall)))
     (if (and (symbolp scope1) (not (null scope1)) (upper-case-p (char (string scope1) 0)))
 	(setq scope1 `',scope1))
-    `(,how ,(#"getIdentifier" name) ,(or scope1 'this) ,@(mapcar 'maybe-class (mapcar 'ast-to-sexp (j2list arguments))))
+    `(,how ,(#"getIdentifier" name) ,(or scope1 'this) ,@(mapcar 'maybe-class 
+								 (mapcar (lambda(el) (ast-to-sexp obj el))
+									 (j2list arguments))))
     ))
 
-(def-javaparse FieldAccessExpr ()
-  (let ((scope (ast-to-sexp (#"getScope" node))))
+(def-java-read FieldAccessExpr sharp-quote-expression-reader ()
+  (let ((scope (ast-to-sexp obj (#"getScope" node))))
     (if (and (symbolp scope) (upper-case-p (char (string scope) 0)))
-	`(get-java-field ',(ast-to-sexp (#"getScope" node)) ,(maybe-class (ast-to-sexp (#"getField" node))))
-	`(get-java-field ,(maybe-class (ast-to-sexp (#"getScope" node))) ,(maybe-class (ast-to-sexp (#"getField" node)))))))
+	`(get-java-field ',(ast-to-sexp obj (#"getScope" node)) ,(#"getIdentifier" (#"getField" node)) t)
+	`(get-java-field ,(maybe-class (ast-to-sexp obj (#"getScope" node))) ,(#"getIdentifier" (#"getField" node)) t))))
 
-(def-javaparse ClassExpr (type)
-  `(find-java-class ',(ast-to-sexp (#"getName" type))))
+(def-java-read ArrayAccessExpr sharp-quote-expression-reader ()
+  (let ((index (ast-to-sexp obj (#"getIndex" node))))
+    (if (symbolp index) (setq index (intern (string-upcase index))))
+    `(aref ,(ast-to-sexp obj (#"getName" node)) ,index)))
 
-(def-javaparse SimpleName ()
+(def-java-read ClassExpr sharp-quote-expression-reader (type)
+  `(find-java-class ',(ast-to-sexp obj (#"getName" type))))
+
+(def-java-read SimpleName sharp-quote-expression-reader ()
   (let ((symbol (intern (#"getIdentifier" node))))
     symbol))
 
-(def-javaparse NameExpr ()
-  (ast-to-sexp (#"getName" node)))
-
+(def-java-read NameExpr sharp-quote-expression-reader ()
+  (ast-to-sexp obj (#"getName" node)))
 
 
 #|(defun def-java-parse-1 (ast-class fields body)
   (register 
   
 			  
-(def-javaparse MethodReferenceExpr (scope typeArguments identifier)
+(def-java-read MethodReferenceExpr (scope typeArguments identifier)
   )
-(def-javaparse Name (identifier qualifier)
+(def-java-read Name (identifier qualifier)
   )
-(def-javaparse NameExpr (name)
+(def-java-read NameExpr (name)
   )
-(def-javaparse ObjectCreationExpr (scope type typeArguments arguments anonymousClassBody)
+(def-java-read ObjectCreationExpr (scope type typeArguments arguments anonymousClassBody)
   )
-(def-javaparse SimpleName (identifier)
+(def-java-read SimpleName (identifier)
   )
-(def-javaparse SingleMemberAnnotationExpr (memberValue)
+(def-java-read SingleMemberAnnotationExpr (memberValue)
   )
-(def-javaparse StringLiteralExpr (value)
+(def-java-read StringLiteralExpr (value)
   )
-(def-javaparse SuperExpr (classExpr)
+(def-java-read SuperExpr (classExpr)
   )
-(def-javaparse ThisExpr (classExpr)
+(def-java-read ThisExpr (classExpr)
   )
-(def-javaparse TypeExpr (type)
+(def-java-read TypeExpr (type)
   )
-(def-javaparse UnaryExpr (expression operator)
+(def-java-read UnaryExpr (expression operator)
   )
-(def-javaparse VariableDeclarationExpr (modifiers annotations variables)
+(def-java-read VariableDeclarationExpr (modifiers annotations variables)
   )
 
 AssignExpr$Operator: (ASSIGN PLUS MINUS MULTIPLY DIVIDE AND OR XOR REMAINDER LEFT_SHIFT SIGNED_RIGHT_SHIFT UNSIGNED_RIGHT_SHIFT codeRepresentation $VALUES)
@@ -153,7 +175,7 @@ com.github.javaparser.ast.expr.NormalAnnotationExpr: (pairs)
 com.github.javaparser.ast.expr.NullLiteralExpr: nil
 com.github.javaparser.ast.expr.ObjectCreationExpr: (scope type typeArguments arguments anonymousClassBody)
 com.github.javaparser.ast.expr.SimpleName: (identifier)
-com.github.javaparser.ast.expr.SingleMemberAnnotationExpr: (memberValue)
+com.github.javaparser.ast.expr.SingleMemberAnnotationExpr: (memberValue)o
 com.github.javaparser.ast.expr.StringLiteralExpr: (value)
 com.github.javaparser.ast.expr.SuperExpr: (classExpr)
 com.github.javaparser.ast.expr.ThisExpr: (classExpr)
@@ -164,41 +186,41 @@ com.github.javaparser.ast.ImportDeclaration: (name isStatic isAsterisk)
 
 com.github.javaparser.ast.expr.
 
-(def-javaparse ArrayAccessExpr (name index)
+(def-java-read ArrayAccessExpr (name index)
   `(aref ,name ,index))
-(def-javaparse ArrayCreationExpr (levels elementType initializer)
+(def-java-read ArrayCreationExpr (levels elementType initializer)
   )
-(def-javaparse ArrayInitializerExpr (values)
+(def-java-read ArrayInitializerExpr (values)
   )
-(def-javaparse AssignExpr (target value operator)
+(def-java-read AssignExpr (target value operator)
   )
-(def-javaparse BinaryExpr (left right operator)
+(def-java-read BinaryExpr (left right operator)
   )
-(def-javaparse CastExpr (type expression)
-  )
-
-(def-javaparse ClassExpr (type)
-  )
-(def-javaparse ConditionalExpr (condition thenExpr elseExpr)
+(def-java-read CastExpr (type expression)
   )
 
-(def-javaparse EnclosedExpr (inner)
+(def-java-read ClassExpr (type)
   )
-(def-javaparse Expression nil
-  )
-(def-javaparse FieldAccessExpr (scope typeArguments name)
-  )
-(def-javaparse InstanceOfExpr (expression type)
+(def-java-read ConditionalExpr (condition thenExpr elseExpr)
   )
 
-(def-javaparse LiteralExpr nil
+(def-java-read EnclosedExpr (inner)
+  )
+(def-java-read Expression nil
+  )
+(def-java-read FieldAccessExpr (scope typeArguments name)
+  )
+(def-java-read InstanceOfExpr (expression type)
   )
 
-(def-javaparse LambdaExpr (parameters isEnclosingParameters body)
+(def-java-read LiteralExpr nil
   )
-(def-javaparse MarkerAnnotationExpr nil
+
+(def-java-read LambdaExpr (parameters isEnclosingParameters body)
   )
-(def-javaparse MemberValuePair (name value)
+(def-java-read MarkerAnnotationExpr nil
+  )
+(def-java-read MemberValuePair (name value)
   )
 
 ;;
