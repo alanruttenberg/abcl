3379176aa1a6c0df90d23a7ad7e8ec49e83c9dc7
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-06-15 09:22:34 -0400

    document == scope (and extend a bit)

--- contrib/jss/transform-to-field.lisp
+++ contrib/jss/transform-to-field.lisp
@@ -1,11 +1,16 @@
 (in-package :jss)
 
 ;; JSS syntax for fields
-;; #"<thing>.<field>"
+;; #"[<scope>]<thing>.<field>"
 ;;
+;; <scope> is empty or "==". scope is only paid attention to when <field> is a literal string
+;; 
 ;; <thing> is either {<lisp expression>} or a class name or abbreviation that find-java-class can use
 ;;   If <thing> is a lisp expression, then it is evaluated (in the lexical environment) and used as an instance
+;;     when <scope> is "==" you promise that instance will always be of the same class, and so field lookup
+;;     is done once and cached. 
 ;;   If <thing> is a class name the result of find-java-class is used and a static field access is done.
+;;     when <scope> is "==" you promise the static field is final and so the result is wrapped in (load-time-value  ...)
 ;;
 ;; <field> is either {<lisp expression} or string
 ;;   If <field> is a lisp expression it should evaluate to a string that names a field
@@ -13,13 +18,14 @@
 ;;
 ;; eg. #"foo.bar.baz" -> (get-java-field (find-java-class 'foo.bar) "baz" t)
 ;;     #"{foo}.baz" -> (get-java-field (find-java-class foo) "baz" t)
-
+;;     #"==foo.baz" -> (load-time-value (get-java-field (find-java-class "foo") "bar" t))
+;;     #"=={foo}.baz" -> TL;DR (only look up baz field once based on class of foo, and cache)
 
 (defun jss-transform-to-field (string sharp-arg)
-  (let* ((pattern (#"compile" 'java.util.regex.Pattern "(={0,2})(.*)\\.([^.]+)$"))
+  (let* ((pattern (#"compile" 'java.util.regex.Pattern "((==){0,1})(.*)\\.([^.]+)$"))
 	 (matcher (#"matcher" pattern string)))
     (#"find" matcher)
-    (let ((parts (list (#"group" matcher 2) (#"group" matcher 3)))
+    (let ((parts (list (#"group" matcher 3) (#"group" matcher 4)))
 	  (scope (#"group" matcher 1)))
       (check-class-or-eval (first parts))
       (check-field-or-eval (second parts))
@@ -40,7 +46,7 @@
 
 (defun field-access-expression (sharp-arg scope thing field )
   (if (and (not (char= (char thing 0) #\{)) (not (char= (char field 0) #\{)))
-      (static-field-ref-transform thing field sharp-arg)
+      (static-field-ref-transform thing field sharp-arg scope)
       (if (and (equal scope "==") (char= (char thing 0) #\{) (not (char= (char field 0) #\{)))
 	  (always-same-signature-field-ref-transform sharp-arg thing field)
 	  `(get-java-field ,(if (char= (char thing 0) #\{)
@@ -52,16 +58,19 @@
 			   t))))
 
 ;; If a class name and explicit field name we can look everything up at load time
-(defun static-field-ref-transform (class field sharp-arg)
-  `(,(if (eql sharp-arg 0) 'jcall-raw 'jcall) (load-time-value (jmethod "java.lang.reflect.Field" "get" "java.lang.Object"))
-	  (load-time-value 
-	     (let ((jfield (find-declared-field ,field (find-java-class ,class))))
-	       (#"setAccessible" jfield t)
-	       jfield))
-    (load-time-value (find-java-class ',class))))
+(defun static-field-ref-transform (class field sharp-arg scope)
+  (if (equal scope "==")
+      `(load-time-value (get-java-field (find-java-class ,class) ,field t))
+      `(,(if (eql sharp-arg 0) 'jcall-raw 'jcall) 
+	(load-time-value (jmethod "java.lang.reflect.Field" "get" "java.lang.Object"))
+	(load-time-value 
+	 (let ((jfield (find-declared-field ,field (find-java-class ,class))))
+	   (#"setAccessible" jfield t)
+	   jfield))
+	(load-time-value (find-java-class ',class)))))
 
-;; 1 case: {var}.foo
-;; Globally cache the field accessor for the first value of {var}. Subsequent calls *ignore* var.
+;; 1 case: =={var}.foo
+;; Globally cache the field accessor for the first value of {var}. Subsequent calls ignore the class of var.
 (defun always-same-signature-field-ref-transform (sharp-arg object field)
   (let ((cached (make-symbol (format nil "CACHED-FIELD-field")))
 	(object (intern (string-upcase (subseq object 1 (- (length object) 1))))))
